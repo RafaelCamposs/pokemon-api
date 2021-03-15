9d281f504a6a7bce4b4841efb845ff97377d1964
Rafael Campos <rafacampos2000@hotmail.com>
2021-03-15 12:59:17 -0300

    User signup, login, profile(secured with JWT) endpoints

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const UserModel = require('./model/model');
+
+mongoose.connect( {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+mongoose.connection.on('error', error => console.log(error) );
+mongoose.Promise = global.Promise;
+
+require('./auth/auth');
+
+const routes = require('./routes/routes');
+const secureRoute = require('./routes/secure-routes');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+app.use('/', routes);
+
+app.use('/user', passport.authenticate('jwt', {session:false}),secureRoute);
+
+app.use(function(err,req,res,next){
+    req.status(err.status || 500 );
+    res.json({error:err});
+});
+
+app.listen(3030,()=>{
+    console.log('Server started')
+})
